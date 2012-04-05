e9311a53f2a5eb6f222518ad75e0d0f26dd30594
dromanov <romanov@rubal.ru>
2012-04-05 19:56:39 +0700

    Empty tests 4 Settings

---
+++ test/unit/settings_test.rb
@@ -0,0 +1,14 @@
+
+require 'test_helper'
+#require 'settings'
+
+# Запуск тестов (набирать из корня проекта): ruby -Itest test/unit/*
+
+class PostTest < ActiveSupport::TestCase
+
+
+  test "load settings" do
+    #settings = Settings.instance
+    assert false
+  end
+end
