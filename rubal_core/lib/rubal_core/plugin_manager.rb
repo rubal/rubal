8d069aa4469137dc81d10cd029ad04007ac88928
Vadim Sibiryanskiy <iwad2d@gmail.com>
2012-04-20 20:07:52 +0700

    PluginManager and RubalTestPlugin was added

---
+++ rubal_core/lib/rubal_core/plugin_manager.rb
@@ -0,0 +1,24 @@
+class PluginManager
+  require 'singleton'
+  module AdminExtend
+
+  end
+  module PageExtend
+
+  end
+  @routes
+  def AddAdminController(given_PluginNameAdminControllerExtend)
+    AdminExtend.send(:include, given_PluginNameAdminControllerExtend)
+  end
+  def AddPageController(given_PluginNamePageControllerExtend)
+    PageExtend.send(:include, given_PluginNamePageControllerExtend)
+  end
+  # добавляем хэш
+  def add_hash= hash
+      @routes.push(hash)
+    return mn
+  end
+  def initialize
+    @routes = Array.new([{"admin/c1/:id" => "admin#call_me_baybe"}, {"admin/i" => "admin#index"}])
+  end
+end
