0548b25ca54fa19866a8ac787366933cf6cdb5f2
dromanov <romanov@rubal.ru>
2012-04-24 21:38:46 +0700

    PluginManager.add_placeholder

--- rubal_core/lib/rubal_core.rb
+++ rubal_core/lib/rubal_core.rb
@@ -1,4 +1,5 @@
 module RubalCore
   require 'rubal_core/settings'
   require 'rubal_core/menu'
+  require 'rubal_core/plugin_manager'
 end
--- rubal_core/lib/rubal_core/plugin_manager.rb
+++ rubal_core/lib/rubal_core/plugin_manager.rb
@@ -1,27 +1,46 @@
 require 'singleton'
 
-class PluginManager
-  include Singleton
+module RubalCore
+  class PluginManager
+    include Singleton
 
-  module AdminExtend
+    attr_reader :routes,:placeholders
 
-  end
-  module PageExtend
+    module AdminExtend
 
-  end
-  @routes
-  def AddAdminController(given_PluginNameAdminControllerExtend)
-    AdminExtend.send(:include, given_PluginNameAdminControllerExtend)
-  end
-  def AddPageController(given_PluginNamePageControllerExtend)
-    PageExtend.send(:include, given_PluginNamePageControllerExtend)
-  end
-  # добавляем хэш
-  def add_hash= hash
-    @routes.push(hash)
-    #return mn
-  end
-  def initialize
-    @routes = Array.new([{"admin/c1/:id" => "admin#call_me_baybe"}, {"admin/i" => "admin#index"}])
+    end
+    module PageExtend
+
+    end
+
+    def initialize
+      @routes = Array.new([{"admin/c1/:id" => "admin#call_me_baybe"}, {"admin/i" => "admin#index"}])
+      @placeholders = {}
+    end
+
+    def AddAdminController(given_PluginNameAdminControllerExtend)
+      AdminExtend.send(:include, given_PluginNameAdminControllerExtend)
+    end
+
+    def AddPageController(given_PluginNamePageControllerExtend)
+      PageExtend.send(:include, given_PluginNamePageControllerExtend)
+    end
+
+    # Add route to CMS
+    # * route  #TODO: add doc
+    def add_route route
+      @routes.push(route)
+    end
+
+    # Add placeholder from plugin to CMS
+    # * category #TODO: add doc
+    # * param #TODO: add doc
+    # * value #TODO: add doc
+    def add_placeholder category, param, value
+      raise TypeError unless (category.instance_of? String) && (param.instance_of? String) && (value.instance_of? String)
+      @placeholders[category]={} unless @placeholders.has_key? category
+      @placeholders[category][param] = value
+    end
+
   end
 end
